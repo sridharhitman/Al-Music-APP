fa4449718773382a408b730ed5278859c7425901
sridharhitman <83956232+sridharhitman@users.noreply.github.com>
2022-05-25 15:26:12 +0530

    v3.0

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristX = 0;
+rightWristY = 0;
+rightWristY = 0;
 function preload(){
     song1 = loadSound('m1.mp3');
     song2 = loadSound('m2.mp3');
@@ -9,7 +13,24 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotResult);
 }
+function modelLoaded() {
+    console.log("model is loaded");
+  }
 function draw(){
     image(video,0,0,600,500);
 }
+function gotResult(r) {
+    if (r.length > 0) {
+      console.log(r);
+      leftWristX = r[0].pose.leftWrist.x
+      leftWristY = r[0].pose.leftWrist.y
+      rightWristX = r[0].pose.rightWrist.x
+      rightWristY = r[0].pose.rightWrist.y
+      console.log('right wrist x ' + rightWristX + 'right wrist y ' + rightWristY )
+      console.log('left wrist x ' + leftWristX + 'left wrist y ' + leftWristY );
+    }
+  }
