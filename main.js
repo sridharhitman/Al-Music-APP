8478713ce5cd73f1cbfe4247e171c80362d63f51
sridharhitman <83956232+sridharhitman@users.noreply.github.com>
2022-05-30 15:41:24 +0530

    v4.0

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX = 0;
 leftWristX = 0;
 rightWristY = 0;
 rightWristY = 0;
+sLeftWrist = 0;
+statusSong1 = ""; 
+statusSong2 = ""; 
 function preload(){
     song1 = loadSound('m1.mp3');
     song2 = loadSound('m2.mp3');
@@ -22,10 +25,23 @@ function modelLoaded() {
   }
 function draw(){
     image(video,0,0,600,500);
+    statusSong1 = song1.isPlaying();
+    statusSong2 = song2.isPlaying();
+    if( sLeftWrist > 0.2){
+      fill('#FF0000');
+      stroke('#FF0000');
+      circle(leftWristX,leftWristY,20)
+      song2.stop();
+      if(statusSong1 == false){
+        song1.play();
+        document.getElementById('songName').innerHTML = 'Harry Potter Theme Song'
+      }
+  }
 }
 function gotResult(r) {
     if (r.length > 0) {
       console.log(r);
+      sLeftWrist = r[0].pose.keypoints[9].score;
       leftWristX = r[0].pose.leftWrist.x
       leftWristY = r[0].pose.leftWrist.y
       rightWristX = r[0].pose.rightWrist.x
